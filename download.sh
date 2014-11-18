871dac00ccc7d1407ccccd2d1157a52f2f7c2af9
Yuandong Tian <yuandong.tian@gmail.com>
2014-11-17 22:59:54 -0800

    Add download.sh

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir intermediate
+cd intermediate
+wget -O camera_ready_smallimages.mat https://www.dropbox.com/sh/yx0cdpe88c4folq/AABV8P5Qzm12_y-R7s0qX0uya/camera_ready_smallimages.mat?dl=0
+wget -O result_camera_ready_Mar23.mat https://www.dropbox.com/sh/yx0cdpe88c4folq/AADhE7gBiW7a4ktgwt5L4nJsa/result_camera_ready_Mar23.mat?dl=0
